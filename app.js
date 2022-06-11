ba182831915db62606dd14c69da8a29d6b13333b
takazo3 <info@re-zero-jp.com>
2022-06-12 00:31:46 +0900

    init

---
+++ app.js
@@ -0,0 +1,46 @@
+Vue.createApp({
+	data() {
+		return {
+			perspective: 100,
+			rotateX: 0,
+			rotateY: 0,
+			rotateZ: 0,
+		};
+	},
+	computed: {
+		box() {
+			return {
+				transform: `
+        perspective(${this.perspective}px)
+        rotateX(${this.rotateX}deg)
+        rotateY(${this.rotateY}deg)
+        rotateZ(${this.rotateZ}deg)`,
+			};
+		},
+	},
+	methods: {
+		reset() {
+			(this.perspective = 100),
+				(this.rotateX = 0),
+				(this.rotateY = 0),
+				(this.rotateZ = 0);
+		},
+		copy() {
+			const el = document.createElement("textarea");
+			el.setAttribute("readonly", "");
+			el.style.left = "-9999px";
+			el.value = `transform: ${this.box.transform}`;
+
+			document.body.appendChild(el);
+			el.select();
+			navigator.clipboard.writeText(
+				"transform: " +
+					document.getElementsByClassName("box")[0].style.transform +
+					";"
+			);
+			alert("Copied!");
+			document.body.removeChild(el);
+			this.reset();
+		},
+	},
+}).mount("#app");
